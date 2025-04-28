56d3b0286148cbe1383c2b1598874e7e94596c52
JerseySchunk <jjsprq@umsystem.edu>
2025-04-28 16:21:50 -0500

    Added skeleton to skeleton :3

--- src/main/java/adoptme/controller/PetController.java
+++ src/main/java/adoptme/controller/PetController.java
@@ -1,5 +1,50 @@
 package adoptme.controller;
 
-public class PetController {
+import adoptme.model.Shelter;
+import adoptme.model.Pet;
+import adoptme.view.PetView;
 
+public class PetController {
+	private Shelter<Pet> shelter;
+	private PetView view;
+	
+	public PetController(Shelter<Pet> shelter, PetView view) {
+		this.shelter = shelter;
+		this.view = view;
+		setupListeners();
+		loadPets();
+		updatePetList();
+	}
+	
+	private void setupListeners() {
+		
+	}
+	
+	private void loadPets() {
+		
+	}
+	
+	private void addPet() {
+		
+	}
+	
+	private void removePet() {
+		
+	}
+	
+	private void adoptPet() {
+		
+	}
+	
+	private void sortPets() {
+		
+	}
+	
+	private void savePets() {
+		
+	}
+	
+	private void updatePetList() {
+		
+	}
 }
