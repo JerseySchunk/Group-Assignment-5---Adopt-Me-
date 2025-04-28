cd7cc0d325781baa64ba054f89e2b0e851ddb134
JerseySchunk <jjsprq@umsystem.edu>
2025-04-28 18:38:49 -0500

    Added basic shelter functions

--- src/main/java/adoptme/model/Shelter.java
+++ src/main/java/adoptme/model/Shelter.java
@@ -1,5 +1,18 @@
 package adoptme.model;
 
-public class Shelter<T extends Pet> {
+import java.util.List;
+import java.util.ArrayList;
 
+public class Shelter<T extends Pet> {
+	
+	private List<T> pets;
+	
+	public Shelter() {
+		pets = new ArrayList<>();
+	}
+
+	public void addPet(T pet) {
+		pets.add(pet);
+	}
+	
 }
