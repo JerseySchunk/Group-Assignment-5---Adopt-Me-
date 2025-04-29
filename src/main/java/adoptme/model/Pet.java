42ff4915f1ff55b5adf4f942bd6fc6eb31fdb8cd
Son <Son@MSI>
2025-04-29 17:50:25 -0500

    flsuhed out pet class more fully.

--- src/main/java/adoptme/model/Pet.java
+++ src/main/java/adoptme/model/Pet.java
@@ -1,16 +1,33 @@
 package adoptme.model;
 
-public abstract class Pet {
+/**
+ *base class representing a pet
+ */
+
+public abstract class Pet implements Comparable <Pet>{
     protected String name;
     protected String species;
     protected int age;
     protected boolean adopted;
 
     public Pet(String name, String species, int age) {
+    	
+    	if (name == null || name.isBlank()) {
+    		throw new IllegalArgumentException("name is blank");
+    	}
+    	if (species == null || species.isBlank())	{
+    		throw new IllegalArgumentException("Species is blank");
+    	}
+    	if (age < 0) {
+    		throw new IllegalArgumentException("age can not be negitive");
+    	}
+    	
         this.name = name;
         this.species = species;
         this.age = age;
         this.adopted = false;
+        
+        
     }
 
     
