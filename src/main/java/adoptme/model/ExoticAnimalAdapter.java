396013eb7dee02a49dee80cf77aab09743ef5d7c
Son <Son@MSI>
2025-04-30 12:28:28 -0500

    adds exotic animal adaptor class

--- src/main/java/adoptme/model/ExoticAnimalAdapter.java
+++ src/main/java/adoptme/model/ExoticAnimalAdapter.java
@@ -2,13 +2,28 @@ package adoptme.model;
 
 import adoptme.thirdparty.ExoticAnimal;
 
+/**
+ * Adapter wraps an ExoticAnimal object and treat it like a standard Pet.
+ * This allows ExoticAnimal instances to work with  with the rest of the system.
+ */
 public class ExoticAnimalAdapter extends Pet {
-    private ExoticAnimal exoticAnimal;
 
-    public ExoticAnimalAdapter(ExoticAnimal exoticAnimal) {
-        super(exoticAnimal.getAnimalName(), exoticAnimal.getCategory(), exoticAnimal.getYearsOld());
-        this.exoticAnimal = exoticAnimal;
+    private ExoticAnimal exotic;
+
+    public ExoticAnimalAdapter(ExoticAnimal exotic) {
+        super(
+            exotic.getAnimalName(),
+            exotic.getSubSpecies() + " (" + exotic.getCategory() + ")",
+            exotic.getYearsOld()
+        );
+        this.exotic = exotic;
     }
 
-}
+    public String getUniqueId() {
+        return exotic.getUniqueId();
+    }
 
+    public ExoticAnimal getWrappedExoticAnimal() {
+        return exotic;
+    }
+}
