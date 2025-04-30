e2887c37085a4b60d70c7ab954cd6ba3e1da4a43
Gideon <gideonmasters09876@gmail.com>
2025-04-29 17:26:29 -0700

    Added Frame and Buttons and Button Listeners

--- src/main/java/adoptme/view/MainFrame.java
+++ src/main/java/adoptme/view/MainFrame.java
@@ -1,5 +1,62 @@
 package adoptme.view;
 
-public class MainFrame {
+import java.awt.event.ActionListener;
 
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+
+public class MainFrame extends JFrame {
+	
+	private JFrame frame;
+	private JTable petTable;
+	private JButton adoptButton,removeButton,viewButton,addButton;
+	
+	public MainFrame() { 
+		//Frame settings
+		frame = new JFrame("Pet Test");
+		setTitle("Pet Adoption Website");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setBounds(300, 100, 500, 400);
+
+		JPanel panel = new JPanel();
+		setContentPane(panel);
+		
+		//Table displaying pets
+		
+		//Buttons
+		
+		addButton = new JButton("Add Pet");
+		adoptButton = new JButton("Adopt a Pet");
+		removeButton = new JButton("Remove a Pet");
+		viewButton = new JButton("View Pet");
+		
+		panel.add(addButton);
+		panel.add(adoptButton);
+		panel.add(removeButton);
+		panel.add(viewButton);
+		
+	}
+		
+		public void addActionListeneradoptButton(ActionListener listener) {
+			adoptButton.addActionListener(listener);
+		}
+		public void addActionListeneraddButton(ActionListener listener) {
+			addButton.addActionListener(listener);
+		}
+		public void addActionListenerremoveButton(ActionListener listener) {
+			removeButton.addActionListener(listener);
+		}
+		public void addActionListenerviewButton(ActionListener listener) {
+			viewButton.addActionListener(listener);
+		}
+		
+		
+		
+		
+		
+		
+		
 }
