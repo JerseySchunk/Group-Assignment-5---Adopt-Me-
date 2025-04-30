63104b8a63fd3b16c37a109ce936d787ccc7a402
Son <Son@MSI>
2025-04-30 10:11:18 -0500

    added dog class extends pet class

--- src/main/java/adoptme/model/Dog.java
+++ src/main/java/adoptme/model/Dog.java
@@ -1,5 +1,20 @@
 package adoptme.model;
 
-public class Dog {
+/**
+ * Represents a dog in the adoption system.
+ * Inherits common pet attributes from Pet.
+ * 
+ * @author joe button
+ */
+public class Dog extends Pet {
 
+    /**
+     * Constructs a new Dog with the given name and age.
+     * 
+     * @param name; the name of the dog
+     * @param age; the age of the dog
+     */
+    public Dog(String name, int age) {
+        super(name, "Dog", age);
+    }
 }
