973b8303b16b569548b0388759435bc33b57dcc1
Son <Son@MSI>
2025-04-30 10:18:52 -0500

    added cat class extends pet class

--- src/main/java/adoptme/model/Cat.java
+++ src/main/java/adoptme/model/Cat.java
@@ -1,5 +1,20 @@
 package adoptme.model;
 
-public class Cat {
+/**
+ * Represents a cat in the adoption system.
+ * Inherits common pet attributes from Pet.
+ * 
+ * @author joe button
+ */
+public class Cat extends Pet {
 
+    /**
+     * Constructs a new Cat with the given name and age.
+     * 
+     * @param name the name of the cat
+     * @param age the age of the cat
+     */
+    public Cat(String name, int age) {
+        super(name, "Cat", age);
+    }
 }
