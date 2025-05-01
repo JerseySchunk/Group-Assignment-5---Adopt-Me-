2a6a157a2b0888f4a1031d9ec12409753e252c0d
JerseySchunk <jjsprq@umsystem.edu>
2025-05-01 14:22:41 -0500

    Fixed logic in sorting again

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,17 @@
+import javax.swing.SwingUtilities;
+import adoptme.model.Shelter;
+import adoptme.model.Pet;
+import adoptme.view.MainFrame;
+import adoptme.controller.PetController;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SwingUtilities.invokeLater(() -> {
+			Shelter<Pet> shelter = new Shelter<>();
+			MainFrame frame = new MainFrame();
+			new PetController(shelter, frame);
+			frame.setVisible(true);
+		});
 	}
-
 }
