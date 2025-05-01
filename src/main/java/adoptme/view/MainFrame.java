b32c7a0fc6b3f4a27642d27335977cb863e1a6c2
JerseySchunk <jjsprq@umsystem.edu>
2025-05-01 14:07:49 -0500

    Method for controller to link to

--- src/main/java/adoptme/view/MainFrame.java
+++ src/main/java/adoptme/view/MainFrame.java
@@ -78,6 +78,8 @@ public class MainFrame extends JFrame {
 			return sortComboBox;
 		}
 
-		
+		public void addActionListenerSaveButton(ActionListener listener) {
+			saveButton.addActionListener(listener);
+		}
 			
 }
