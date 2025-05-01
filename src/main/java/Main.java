815acf6b246bf19e6de371e12dd304571aec00ec
JerseySchunk <jjsprq@umsystem.edu>
2025-05-01 14:26:17 -0500

    Added documentation to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,24 @@ import adoptme.model.Pet;
 import adoptme.view.MainFrame;
 import adoptme.controller.PetController;
 
+/*
+ * The start of the program!
+ * 
+ * Will initialize the components of the application while
+ * following the MVC expectations. 
+ */
+
 public class Main {
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> {
+			//This is the Model
 			Shelter<Pet> shelter = new Shelter<>();
+			//This is the view
 			MainFrame frame = new MainFrame();
+			//This is the controller
 			new PetController(shelter, frame);
+			//This makes the GUI visible
 			frame.setVisible(true);
 		});
 	}
